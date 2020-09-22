9c8561a5b74b36235c85276e145880a8946f772d
Ron Kantor <ron.kantor@theculturetrip.com>
2020-09-22 09:23:09 +0300

    update readme

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Mancala game powered by react for gui, express for server side and prolog for the engine.
 
-The project requires you to have swi-prolog installed and have `swipl` inside the PATH.
+The project requires you to have swi-prolog installed and have `swipl` in PATH.
 
 ### How to setup
 
@@ -12,9 +12,36 @@ npm run project:setup
 
 ### Run project
 
-The GUI and the server are two different server, inorder to get them up and running you will need to run the following commands from the root directory.
+The GUI and the server are two different server, inorder to get them up and running you will need to run the following commands in different terminals.
 
 ```shell
 npm run project:start:gui
 npm run project:start:server
 ```
+
+### TODO:
+
+
+#### Prolog engine
+
+- [ ] Implement rule: if last pebble is in an empty pit, take the oppsite pebbels
+- [ ] Make the game interactive
+- [ ] Change to display function to return variables instead of outputing the board
+- [ ] Refactor functions to be more clean
+- - [ ] `LastT` & `take` are doing the same but behieve very differntly
+- [ ] Document everything
+
+- [ ] ** STICH EVERYTHING TOGETHER **
+
+#### GUI
+
+- [ ] Starter screen
+- [ ] End screen
+- [ ] Option to restart to game while playing(not a must)
+
+#### Server
+
+- [ ] Connect node app to prolog code
+- [ ] Endpoint to change difficulty
+- [ ] Endpoint to make the user move
+- [ ] Endpoint to get the AI move
--- server/app.js
+++ server/app.js
@@ -7,28 +7,30 @@ const app = express();
 const port = 5000;
 
 app.get("/test", async (req, res) => {
-    const result = await engine.call("member(X, [1,2,3,4])");
-	
-    if (result) {
-		res.send(`Variable X value is: ${result.X}`);
-    } else {
-		res.send("Call failed.");
-    }
-    // Either run more queries or stop the engine.
-    engine.close();
+  const result = await engine.call("member(X, [1,2,3,4])");
+
+  if (result) {
+    res.send(`Variable X value is: ${result.X}`);
+  } else {
+    res.send("Call failed.");
+  }
+  // Either run more queries or stop the engine.
+  engine.close();
 });
 
 app.get("/difficulty/:level", async (req, res) => {
-	await engine.call("working_directory(_,ai)");
-	await engine.call('consult(test)');
+  await engine.call("working_directory(_,ai)");
+  await engine.call('consult(test)');
 
-	const result = await engine.call("test(X, react)");
-	
-    if (result) {
-		res.send(`Variable X value is: ${result.X}`);
-    } else {
-		res.send("Call failed.");
-    }
+  const result = await engine.call("test(X, react)");
+
+  if (result) {
+    res.send(`Variable X value is: ${result.X}`);
+  } else {
+    res.send("Call failed.");
+  }
+  
+  engine.close();
 });
 
 app.post("/play", async (req, res) => {
@@ -36,5 +38,5 @@ app.post("/play", async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
