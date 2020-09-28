05c622e9e51a403273a093e5bf6072f2906c3a61
Ron Kantor <ron.kantor@theculturetrip.com>
2020-09-28 17:28:58 +0300

    set up move endpoint

--- server/app.js
+++ server/app.js
@@ -1,40 +1,78 @@
+const { findAllByAltText } = require("@testing-library/react");
 const express = require("express");
 const swipl = require("swipl-stdio");
-
-const engine = new swipl.Engine();
+const { board_to_array } = require("./helpers.js");
+const helpers = require("./helpers.js");
 
 const app = express();
 const port = 5000;
 
-app.get("/test", async (req, res) => {
-  const result = await engine.call("member(X, [1,2,3,4])");
+const engine = new swipl.Engine();
 
-  if (result) {
-    res.send(`Variable X value is: ${result.X}`);
-  } else {
-    res.send("Call failed.");
-  }
-  // Either run more queries or stop the engine.
-  engine.close();
-});
-
-app.get("/difficulty/:level", async (req, res) => {
+(async () => {
   await engine.call("working_directory(_,ai)");
-  await engine.call('consult(test)');
+  await engine.call('consult(main)');
+})();
 
-  const result = await engine.call("test(X, react)");
+app.use(express.json());
 
-  if (result) {
-    res.send(`Variable X value is: ${result.X}`);
-  } else {
-    res.send("Call failed.");
-  }
-  
-  engine.close();
+app.get("/play", async (req, res) => {
+  // init game
 });
 
-app.post("/play", async (req, res) => {
+app.post("/move", async (req, res) => {
+  const { board, move } = req.body;
+  const depth = req.body.depth || 1;
 
+  const response = {
+    'succuss': true,
+    'board': [],
+    'winner': null,
+    'player': {
+      'extraTurn': false
+    },
+    'ai': {
+      'moves': []
+    }
+  }
+
+  try {
+    var query = `play(${helpers.array_to_string(board)}, player, ${move}, AfterBoard, ExtraTurn, Winner)`;
+    var { AfterBoard, ExtraTurn, Winner } = await engine.call(query);
+  } catch (error) {
+    res.status(400).json({ 'succuss': false, 'error': error })
+    engine.close();
+    return;
+  }
+
+  response.board.push(helpers.board_to_array(AfterBoard));
+
+  if (Winner !== null) {
+    response.winner = Winner;
+    res.json(response);
+    engine.close();
+    return;
+  }
+
+  if (ExtraTurn) {
+    resoinse.player.extraTurn = true;
+    res.json(response);
+    return;
+  }
+
+  try{
+    var queryAI = `play(${helpers.board_to_string(AfterBoard)}, ai, ${depth}, FinalBoard, Moves, WinnerAI)`;
+    var { FinalBoard, Moves, WinnerAI } = await engine.call(queryAI);
+  } catch(error){
+    res.status(400).json({ 'succuss': false, 'error': error })
+    engine.close();
+    return;
+  }
+
+  response.winner = WinnerAI;
+  response.board.push(helpers.board_to_array(FinalBoard));
+  response.ai.moves = helpers.flatten_list(Moves);
+  res.json(response);
 });
 
 app.listen(port, () => {
