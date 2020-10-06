4f8dfb93a1c173777a56de8a65438af8aef4a770
Ron Kantor <ron.kantor@theculturetrip.com>
2020-10-06 17:22:44 +0300

    reformat

--- server/ai/Facts.pl
+++ server/ai/Facts.pl
@@ -3,26 +3,23 @@ Facts of the Game
 */
 :- dynamic settingsDepth/1.
 
-
-/* Non-zero list means */
-non_zero(Hs) :- Hs \== [0,0,0,0,0,0].
-
-/* Zero list means */
-zero([0,0,0,0,0,0]).
+/* Change player */
+next_player(ai, player).
+next_player(player, ai).
 
 /* Set game difficulty */
 settings(treeDepth, 1, easy).
 settings(treeDepth, 3, medium).
 settings(treeDepth, 5, hard).
 
+/* Non-zero list means */
+non_zero(X) :- X \== [0,0,0,0,0,0].
 
+/* Zero list means */
+zero([0,0,0,0,0,0]).
 
 /* Set number of stones */
 settings(stones, 1).
 
 /* Set duration time */
 settings(pauseDuration, 2).
-
-/* Change player */
-next_player(ai, player).
-next_player(player, ai).
